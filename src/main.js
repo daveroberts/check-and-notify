d6d681ef2a8081ca2fbdf47f45b673c2c5631793
Dave Roberts <dave.a.roberts@gmail.com>
2023-04-01 06:51:24 -0400

    Add Kindercare reminder

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ const transporter = nodemailer.createTransport({
 const DAY_SUNDAY = 0;
 const DAY_WEDNESDAY = 3;
 const DAY_THURSDAY = 4;
+const DAY_SATURDAY = 6;
 const HOUR_6AM = 10;
 const HOUR_12PM = 16;
 const HOUR_6PM = 22;
@@ -27,6 +28,7 @@ const HOUR_6PM = 22;
   if (day_of_week == DAY_WEDNESDAY && hour == HOUR_6PM){ await send_email("Task: Take out trash") }
   if (day_of_month == 28 && hour == HOUR_6AM){ await send_email("Task: Pay Citi CC Bill") }
   if (day_of_week == DAY_THURSDAY && hour == HOUR_12PM){ await send_email("Task: Epic Games free game of the week") }
+  if (day_of_week == DAY_SATURDAY && hour == HOUR_12PM){ await send_email("Task: Download Kindercare Pictures") }
 })();
 
 async function send_email(subject){
